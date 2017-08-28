cf5038ff7557fefa8b826260c83962f964680834
Kevin D.Hall <Kevin@khallware.com>
2017-08-28 15:39:33 -0500

    not possible to list all classes in the class loader

--- j2ee/jsr250-annotations/Main.java
+++ j2ee/jsr250-annotations/Main.java
@@ -3,8 +3,6 @@ package com.khallware.jsr250;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Annotation;
 import java.lang.annotation.RetentionPolicy;
-import org.reflections.Reflections;
-import java.util.Arrays;
 import java.util.Map;
 
 /**
@@ -13,35 +11,35 @@ import java.util.Map;
  * mvn -Dmaven.repo.local=$POC_MAVEN_REPO \
  *     org.apache.maven.plugins:maven-dependency-plugin:2.1:get \
  *     -DrepoUrl=url \
- *     -Dartifact=org.reflections:reflections:0.9.11
- * REFL_JAR=$(find $POC_MAVEN_REPO -name \*reflections\*jar)
- * javac -d /tmp -cp $REFL_JAR Main.java
+ *     -Dartifact=javax.annotation:javax.annotation-api:1.3
+ * ANNOT_JAR=$(find $POC_MAVEN_REPO -name \*annot\*jar)
+ * javac -d /tmp -cp $ANNOT_JAR Main.java
  * echo 'Main-Class: com.khallware.jsr250.Main' >/tmp/manifest
  * jar cmvf /tmp/manifest /tmp/jsr250-poc.jar -C /tmp com
  * rm -rf /tmp/com /tmp/manifest
  *
- * mvn -Dmaven.repo.local=$POC_MAVEN_REPO \
- *     org.apache.maven.plugins:maven-dependency-plugin:2.1:get \
- *     -DrepoUrl=url \
- *     -Dartifact=com.google.guava:guava:23.0
- * mvn -Dmaven.repo.local=$POC_MAVEN_REPO \
- *     org.apache.maven.plugins:maven-dependency-plugin:2.1:get \
- *     -DrepoUrl=url \
- *     -Dartifact=org.javassist:javassist:3.22.0-CR2
- * JAR1=$(find $POC_MAVEN_REPO -name \*google\*jar)
- * JAR2=$(find $POC_MAVEN_REPO -name \*javassist\*jar)
- * java -cp /tmp/jsr250-poc.jar:$REFL_JAR:$JAR1:$JAR2 com.khallware.jsr250.Main
+ * java -cp /tmp/jsr250-poc.jar:$ANNOT_JAR com.khallware.jsr250.Main
  * rm -rf /tmp/jsr250-poc.jar $POC_MAVEN_REPO
  */
 public class Main
 {
 	private static Map<String,String> map = new java.util.HashMap<>();
 
+	// from wikipedia.org (jsr_250)
 	static {
-		map.put("Generated",
-			"code written externally by utilities");
-		map.put("Resource",
-			"refers to an external resource (eg database)");
+		map.put("javax.annotation.Generated", "Marks sources that have been generated");
+		map.put("javax.annotation.Resource", "Declares a reference to a resource, e.g. a database");
+		map.put("javax.annotation.Resources", "Container for multiple Resource annotations");
+		map.put("javax.annotation.PostConstruct", "Is used on methods that need to get executed after dependency injection is done to perform any initialization.");
+		map.put("javax.annotation.PreDestroy", "Is used on methods that are called before the instance is removed from the container");
+		map.put("javax.annotation.Priority", "Is used to indicate in what order the classes should be used. For, e.g., the Interceptors specification defines the use of priorities on interceptors to control the order in which interceptors are called.");
+		map.put("javax.annotation.security.RunAs", "Defines the role of the application during execution in a Java EE container");
+		map.put("javax.annotation.security.RolesAllowed", "Specifies the security roles permitted to access method(s) in an application.");
+		map.put("javax.annotation.security.PermitAll", "Specifies that all security roles are permitted to access the annotated method, or all methods in the annotated class.");
+		map.put("javax.annotation.security.DenyAll", "Specifies that no security roles are allowed to invoke the specified method(s).");
+		map.put("javax.annotation.security.DeclareRoles", "Used to specify the security roles by the application.");
+		map.put("javax.annotation.sql.DataSourceDefinition", "Is used to define a container DataSource and be registered with JNDI. The DataSource may be configured by setting the annotation elements for commonly used DataSource properties.");
+		map.put("javax.annotation.ManagedBean", "Is used to declare a Managed Bean which are container managed objects that support a small set of basic services such as resource injection, lifecycle callbacks and interceptors.");
 	};
 
 	@Retention(RetentionPolicy.RUNTIME)
@@ -49,24 +47,11 @@ public class Main
 
 	public static void main(String... args) throws Exception
 	{
-		String name = null;
-		String[] list = new String[] {
-			"javax.annotation", "com.khallware.jsr250",
-			"java.lang"
-		};
-		for (String pkg : list) {
-			Reflections r = new Reflections(pkg);
-			System.out.printf("Annotations in package: %s (%d)\n",
-				pkg, r.getSubTypesOf(Annotation.class).size());
+		ClassLoader cl = ClassLoader.getSystemClassLoader();
 
-			for (Class clazz : r.getSubTypesOf(Annotation.class)) {
-				name = clazz.getSimpleName();
-				System.out.printf("\t%s - %s\n",
-					name,
-					(map.containsKey(name))
-						? map.get(name)
-						: "not JSR250");
-			}
+		for (String key : map.keySet()) {
+			Class clazz = cl.loadClass(key);
+			System.out.printf("\t%s - %s\n", key, map.get(key));
 		}
 	}
 }
